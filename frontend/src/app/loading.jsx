b4ef66926d1cf4c0a2fbffd97b599ff75034405e
Ana <apalac13>
2025-03-20 00:52:20 +0100

    loader added

---
+++ frontend/src/app/loading.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+export default function Loading() {
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowOverlay(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {showOverlay && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black-40 z-[9999] flex justify-center items-center">
+          <div className="animate-pulse ">
+            <Image
+              src="/icons/ASInstruktorLogoOrginalBW.svg"
+              alt="Autoškola Instruktor logo"
+              width={500}
+              height={500}
+              className="object-contain"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
