d984fb426441997b9fd1778df83506d2fa29cc31
apalac13 <anabradaric77@gmail.com>
2025-11-19 15:15:36 +0100

    fix mail

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,5 +1,5 @@
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.EMAIL_PASS);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async ({ to, subject, html }) => {
   try {
