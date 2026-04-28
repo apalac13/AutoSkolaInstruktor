a7c12f78ca533d270007ca78149d65b4d607def9
apalac13 <anabradaric77@gmail.com>
2026-04-28 17:33:17 +0200

    fixed email

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendEmail = async ({ to, subject, html, replyTo }) => {
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "info@autoskolainstruktor.com",
       to,
       subject,
       html,
